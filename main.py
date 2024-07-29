a91c669a0e9d4edfb2881c935a10c98980aa5f7a
MArney86 <matthew.h.arney.1986@gmail.com>
2024-07-28 19:22:35 -0500

    commenting of code and minor fixes

--- main.py
+++ main.py
@@ -34,14 +34,14 @@ def main():
         elif choice == '4': #chose genre operations
             genre_menu() #load the genre menu
         elif choice == '5': #chose to quit
-            file_support.save_all(library_authors, library_books, library_genres, library_users)
+            file_support.save_all(library_authors, library_books, library_genres, library_users) #save all information to file
             sys.exit("Thank you for using the Library Management System") #exit program to system/terminal prompt
         elif choice == 'Force Reload':
-            file_support.reload_all(library_authors, library_books, library_genres, library_users)
-            print("\033[7mAll information has been cleared and reloaded from file\033[0m")
+            file_support.reload_all(library_authors, library_books, library_genres, library_users) #clear and reload all information from file
+            print("\033[7mAll information has been cleared and reloaded from file\033[0m") #notification of reload
         elif choice == 'Force Save':
-            file_support.save_all(library_authors, library_books, library_genres, library_users)
-            print("\033[7mAll information has been saved to file by operator request\033[0m")
+            file_support.save_all(library_authors, library_books, library_genres, library_users) #force save all information to file
+            print("\033[7mAll information has been saved to file by operator request\033[0m") #notification of forced save
         else: #invalid response
             print("Invalid choice. Please try again.") #notify user of invalid response
             
@@ -57,21 +57,21 @@ def book_menu():
 
         choice = input("What would you like to do?: ") #get user choice
         if choice == '1': #chose to add book
-            key, genre_bool, genre = bu.add_book(library_books, library_genres) #add book and store returned key
-            file_support.update_books(library_books, key)
-            if genre_bool:
-                file_support.update_genres(library_genres, genre)
+            key, genre_bool, genre = bu.add_book(library_books, library_genres) #add book and store returned key and info about whether genres were updated
+            file_support.update_books(library_books, key) #save new entry to file
+            if genre_bool: #if genres were updated during book addition
+                file_support.update_genres(library_genres, genre) #save new genre to file as well
 
         elif choice == '2': #chose to borrow a book
             success = bu.borrow_book(library_users, library_books) #borrow book
-            if success:
-                file_support.save_books(library_books)
-                file_support.save_users(library_users)
+            if success: #if borrow successful
+                file_support.save_books(library_books) #save books with updated information
+                file_support.save_users(library_users) #save users with updated information
         elif choice == '3': #chose to return a book
             success = bu.return_book(library_users, library_books) #return book
-            if success:
-                file_support.save_books(library_books)
-                file_support.save_users(library_users)
+            if success: #if return successful
+                file_support.save_books(library_books) #save books with updated information
+                file_support.save_users(library_users) #save users with updated information
         elif choice == '4': #chose to search for a book
             bu.search_books(library_books) #search books
         elif choice == '5': #chose to display all books
@@ -93,7 +93,7 @@ def user_menu():
         choice = input("What would you like to do?: ").strip() #get user choice
         if choice == '1': #chose to add user
             key = uu.add_user(library_users) #add user
-            file_support.update_users(library_users, key)
+            file_support.update_users(library_users, key) #save new user to file
         elif choice == '2': #view a user's details
             uu.view_user_details(library_users, library_books) #view user details
         elif choice == '3': #display all users
@@ -114,7 +114,7 @@ def author_menu():
         choice = input("What would you like to do?: ").strip() #get user choice
         if choice == '1': #chose to add user
             key = au.add_author(library_authors) #add user
-            file_support.update_authors(library_authors, key)
+            file_support.update_authors(library_authors, key) #save new author to file
         elif choice == '2': #view a user's details
             au.view_author_details(library_authors) #view user details
         elif choice == '3': #display all users
@@ -135,7 +135,7 @@ def genre_menu():
         choice = input("What would you like to do?: ").strip() #get user choice
         if choice == '1': #chose to add genre
             key = gu.add_genre(library_genres) #add genre
-            file_support.update_genres(library_genres, key)
+            file_support.update_genres(library_genres, key) #save new genre to file
         elif choice == '2': #view a genre's details
             gu.view_genre_details(library_genres) #view genre details
         elif choice == '3': #display all genres
